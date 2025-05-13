f6b1e29f480c6336085dd1e12cdec4f2c4208d7e
sergeykraynov <kraynov.sergey@gmail.com>
2025-05-13 14:01:00 +0300

    Final fix

--- dist/js/app.js
+++ dist/js/app.js
@@ -9651,22 +9651,6 @@ ScrollTrigger_getGSAP() && ScrollTrigger_gsap.registerPlugin(ScrollTrigger_Scrol
 
 gsapWithCSS.registerPlugin(ScrollTrigger_ScrollTrigger);
 
-const folderOperations = document.querySelector('[data-folder-operations]');
-if (folderOperations) {
-    gsapWithCSS.to(folderOperations, {
-        y: 0,
-        duration: 1,
-        delay: 1,
-        ease: 'power4.out',
-        scrollTrigger: {
-            trigger: folderOperations,
-            start: 'top 100%',
-            end: 'top 20%',
-            toggleActions: 'play reverse play reverse'
-        }
-    });
-}
-
 const lines = document.querySelectorAll('[data-lines]');
 if (lines.length > 0) {
     lines.forEach(item => {
--- src/js/modules/gsap.js
+++ src/js/modules/gsap.js
@@ -3,22 +3,6 @@ import { ScrollTrigger } from "gsap/ScrollTrigger.js";
 
 gsap.registerPlugin(ScrollTrigger);
 
-const folderOperations = document.querySelector('[data-folder-operations]');
-if (folderOperations) {
-    gsap.to(folderOperations, {
-        y: 0,
-        duration: 1,
-        delay: 1,
-        ease: 'power4.out',
-        scrollTrigger: {
-            trigger: folderOperations,
-            start: 'top 100%',
-            end: 'top 20%',
-            toggleActions: 'play reverse play reverse'
-        }
-    });
-}
-
 const lines = document.querySelectorAll('[data-lines]');
 if (lines.length > 0) {
     lines.forEach(item => {
